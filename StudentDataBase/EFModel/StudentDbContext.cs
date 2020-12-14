7314cf0435c656e247fb6a105a0e18c73d09dc6f
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-14 16:56:41 +0800

    Student数据库上下文类

---
+++ StudentDataBase/EFModel/StudentDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement_DataBase.ModelExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement_DataBase.EFModel
+{
+	public class StudentDbContext : IdentityDbContext
+	{
+		public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Students> Students { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Seed();
+		}
+	}
+}
