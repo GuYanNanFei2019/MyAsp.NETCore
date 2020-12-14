c300efae1a7be57bd637b74da8f3d3c5cb9f1a25
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-14 16:58:49 +0800

    数据库迁移记录

---
+++ StudentDataBase/Migrations/20201205072238_SeedStudentData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentManagement_DataBase.Migrations
+{
+    public partial class SeedStudentData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "ID", "ClassName", "Email", "Name" },
+                values: new object[] { 1, 4, "MoshangPengyou@hotmail.com", "朱超" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
