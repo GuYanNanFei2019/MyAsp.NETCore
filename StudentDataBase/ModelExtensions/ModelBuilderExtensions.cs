9ad3b40b52468e178f1c437da9f5a794fdefc3a0
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-14 17:00:10 +0800

    数据库种子数据类

---
+++ StudentDataBase/ModelExtensions/ModelBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement_DataBase.EFModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagement_DataBase.ModelExtensions
+{
+	public static class ModelBuilderExtensions
+	{
+		public static void Seed(this ModelBuilder modelBuilder) 
+		{
+			modelBuilder.Entity<Students>().HasData(
+				data: new Students
+				{
+					ID = 1,
+					Name = "朱超",
+					ClassName = ClassNameEnum.FourthGrade,
+					Email = "MoshangPengyou@hotmail.com",
+					PhotoPath=null
+				}
+				);
+		}
+	}
+}
