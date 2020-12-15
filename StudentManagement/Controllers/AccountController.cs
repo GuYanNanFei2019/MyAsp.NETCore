d46440c92d39ac90768f0985ad30af84ddada59c
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-15 17:17:06 +0800

    设置登录后跳转到ReturnUrl和防止开放式重定向漏洞

--- StudentManagement/Controllers/AccountController.cs
+++ StudentManagement/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
 using StudentManagement.ViewModels.IdentityViewModel;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,7 @@ namespace StudentManagement.Controllers
 		private readonly UserManager<IdentityUser> _userManager;
 		private readonly SignInManager<IdentityUser> _signInManager;
 
-		public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+		public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
 		{
 			_userManager = userManager;
 			_signInManager = signInManager;
@@ -26,7 +28,7 @@ namespace StudentManagement.Controllers
 		/// </summary>
 		/// <returns></returns>
 		[HttpGet]
-		public IActionResult Register() 
+		public IActionResult Register()
 		{
 			return View();
 		}
@@ -37,7 +39,7 @@ namespace StudentManagement.Controllers
 		/// <param name="model"></param>
 		/// <returns></returns>
 		[HttpPost]
-		public async Task<IActionResult> Register(RegistryViewModel model) 
+		public async Task<IActionResult> Register(RegistryViewModel model)
 		{
 			if (ModelState.IsValid)
 			{
@@ -70,27 +72,42 @@ namespace StudentManagement.Controllers
 		/// </summary>
 		/// <returns></returns>
 		[HttpPost]
-		public async Task<IActionResult> LogOut() 
+		public async Task<IActionResult> LogOut()
 		{
 			await _signInManager.SignOutAsync();
 			return RedirectToAction("Index", "Home");
 		}
 
 		[HttpGet]
-		public IActionResult Login() 
+		public IActionResult Login(string ReturnUrl)
 		{
-			return View();
+			LoginViewModel model = new LoginViewModel
+			{
+				ReturnUrl = ReturnUrl
+			};
+
+			return View(model);
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> Login(LoginViewModel model) 
+		public async Task<IActionResult> Login(LoginViewModel model, string ReturnUrl)
 		{
 			if (ModelState.IsValid)
 			{
 				var res = await _signInManager.PasswordSignInAsync(model.Email, model.PassWord, model.RememberMe, false);
 				if (res.Succeeded)
 				{
-					return RedirectToAction("Index", "Home");
+					if (!string.IsNullOrWhiteSpace(ReturnUrl))
+					{
+						if (Url.IsLocalUrl(ReturnUrl))
+						{
+							return Redirect(ReturnUrl);
+						}
+					}
+					else
+					{
+						return RedirectToAction("Index", "Home");
+					}
 				}
 
 				ModelState.AddModelError(string.Empty, "登录失败，请重试");
