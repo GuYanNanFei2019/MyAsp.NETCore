d5cfd6fe7e717d86556f70678aa8428a108cf72a
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-15 15:29:45 +0800

    允许匿名访问Account控制器

--- StudentManagement/Controllers/AccountController.cs
+++ StudentManagement/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StudentManagement.ViewModels.IdentityViewModel;
 using System;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace StudentManagement.Controllers
 {
+	[AllowAnonymous]
 	public class AccountController : Controller
 	{
 		private readonly UserManager<IdentityUser> _userManager;
