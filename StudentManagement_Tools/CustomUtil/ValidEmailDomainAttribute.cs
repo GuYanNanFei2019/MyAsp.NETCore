29a457a2566adc141ca82ebdb6c6db6ebf1723bd
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-16 16:07:36 +0800

    自定义邮箱后缀验证

---
+++ StudentManagement_Tools/CustomUtil/ValidEmailDomainAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudentManagement_Tools.CustomUtil
+{
+	public class ValidEmailDomainAttribute:ValidationAttribute
+	{
+		private readonly string AllowDomain;
+
+		public ValidEmailDomainAttribute(string allowdomain)
+		{
+			AllowDomain = allowdomain;
+		}
+
+		public override bool IsValid(object value)
+		{
+			string[] strings = value.ToString().Split('@');
+
+			return strings[1].ToUpper() == AllowDomain.ToUpper();
+		}
+	}
+}
