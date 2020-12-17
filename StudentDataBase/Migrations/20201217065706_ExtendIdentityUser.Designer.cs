1abcb2b90ee7a3f7e7c55636612fe8e2b5136715
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-17 15:13:27 +0800

    扩展IdentityUser类，添加City属性

--- StudentDataBase/Migrations/20201217065706_ExtendIdentityUser.Designer.cs
+++ StudentDataBase/Migrations/20201217065706_ExtendIdentityUser.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentManagement_DataBase.EFModel.StudentModel;
 
 namespace StudentManagement_DataBase.Migrations
 {
     [DbContext(typeof(StudentDbContext))]
-    partial class StudentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201217065706_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
