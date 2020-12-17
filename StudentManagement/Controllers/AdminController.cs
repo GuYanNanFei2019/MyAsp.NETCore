c84aa372f53f380b62dd25df647b5574c7ad920a
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-17 15:55:23 +0800

    Admin控制器，新增角色方法

---
+++ StudentManagement/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using StudentManagement.ViewModels.IdentityViewModel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+	public class AdminController : Controller
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AdminController(RoleManager<IdentityRole> roleManager) => _roleManager = roleManager;
+
+		[HttpGet]
+		public IActionResult CreateRole()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateRole(CreateRoleViewModel viewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var role = new IdentityRole
+				{
+					Name = viewModel.RoleName
+				};
+
+				var res = await _roleManager.CreateAsync(role);
+				if (res.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+
+				foreach (var item in res.Errors)
+				{
+					ModelState.AddModelError(string.Empty, item.Description);
+				}
+			}
+
+			return View(viewModel);
+		}
+	}
+}
