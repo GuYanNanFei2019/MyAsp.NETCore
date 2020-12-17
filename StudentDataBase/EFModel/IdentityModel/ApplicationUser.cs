549feb640c0e1ef234df14eb6fb255695df89d19
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-17 15:19:16 +0800

    新增扩展IdentityUser类

---
+++ StudentDataBase/EFModel/IdentityModel/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagement_DataBase.EFModel.IdentityModel
+{
+	public class ApplicationUser:IdentityUser
+	{
+		public string City { get; set; }
+	}
+}
