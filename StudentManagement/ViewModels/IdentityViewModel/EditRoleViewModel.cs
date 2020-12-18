95d964b555b03c75aa285fc3d93e174dfe118b3d
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-18 17:36:02 +0800

    编辑角色视图模型

---
+++ StudentManagement/ViewModels/IdentityViewModel/EditRoleViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModels.IdentityViewModel
+{
+	public class EditRoleViewModel
+	{
+		public EditRoleViewModel()
+		{
+			Users =new List<string>();
+		}
+
+		[Display(Name ="角色ID")]
+		public string Id { get; set; }
+
+		[Required(ErrorMessage ="角色名称不得为空")]
+		[Display(Name ="角色名称")]
+		public string RoleName { get; set; }
+
+		[Display(Name ="该角色中的用户")]
+		public List<string> Users { get; set; }
+	}
+}
