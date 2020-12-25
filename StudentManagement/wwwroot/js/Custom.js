18d5203aee475367c6aa747830f1838c9d5a884e
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-25 16:30:35 +0800

    删除用户确认JS

---
+++ StudentManagement/wwwroot/js/Custom.js
@@ -0,0 +1,14 @@
+﻿function confirmDelete(uniqueId,isDeleteCliked)
+{
+	var deletespan = "deleteSpan_" + uniqueId;
+	var confirmDeleteSpan = "confirmdelete_" + uniqueId;
+
+	if (isDeleteCliked) {
+		$("#" + deletespan).hide();
+		$("#" + confirmDeleteSpan).show();
+
+	} else {
+		$("#" + deletespan).show();
+		$("#" + confirmDeleteSpan).hide();
+	}
+}
