973bae387b006d62ef1b40673abfb8f21f0bc372
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-27 15:40:21 +0800

    用户所属角色视图模型

---
+++ StudentManagement/ViewModels/IdentityViewModel/UserRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentManagement.ViewModels.IdentityViewModel
+{
+	public class UserRoleViewModel
+	{
+		public string UserId { get; set; }
+
+		public string UserName { get; set; }
+
+		public bool IsSelected { get; set; }
+	}
+}
