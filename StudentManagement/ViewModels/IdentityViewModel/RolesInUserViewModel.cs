dcc327ea13805652fefce6c0cd5ccfdec1cff4b2
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-27 17:40:48 +0800

    用户的角色视图模型

---
+++ StudentManagement/ViewModels/IdentityViewModel/RolesInUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModels.IdentityViewModel
+{
+	public class RolesInUserViewModel
+	{
+		public string RoleId { get; set; }
+
+		public string RoleName { get; set; }
+
+		public bool IsSelected { get; set; }
+	}
+}
