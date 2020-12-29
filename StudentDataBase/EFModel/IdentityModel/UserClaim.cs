569778b69a8e4ce4935770a4ed36643dba78edce
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-29 10:59:49 +0800

    用户声明与声明类型

---
+++ StudentDataBase/EFModel/IdentityModel/UserClaim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement_DataBase.EFModel.IdentityModel
+{
+    public class UserClaim
+    {
+		public string ClaimType { get; set; }
+
+		public bool IsSelected { get; set; }
+	}
+}
---
+++ StudentDataBase/ModelExtensions/ClaimsStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement_DataBase.ModelExtensions
+{
+    public class ClaimsStore
+    {
+        public static List<Claim> AllClaim = new List<Claim>()
+        {
+            new Claim("Create Role","Create Role"),
+            new Claim("Edit Role","Edit Role"),
+            new Claim("Delete Role","Delete Role"),
+            new Claim("EditStudent","EditStudent"),
+        };
+    }
+}
