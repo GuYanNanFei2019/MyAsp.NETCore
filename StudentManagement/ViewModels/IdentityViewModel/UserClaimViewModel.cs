c2819343239a9ba7d9708857267f007b9d077de5
814895493@qq.com <moshangpengyou@hotmail.com>
2020-12-29 10:56:49 +0800

    用户声明视图模型

---
+++ StudentManagement/ViewModels/IdentityViewModel/UserClaimViewModel.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagement_DataBase.EFModel.IdentityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModels.IdentityViewModel
+{
+    public class UserClaimViewModel
+    {
+		public UserClaimViewModel()
+		{
+			UserClaims = new List<UserClaim>();
+		}
+
+		public string UserId { get; set; }
+
+		public List<UserClaim> UserClaims { get; set; }
+	}
+}
